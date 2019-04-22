dc0a11873551c885cbafb47fc9592151882dd39b
IchiyaParfum <37086640+IchiyaParfum@users.noreply.github.com>
2019-04-22 16:37:44 +0200

    Added Unity Project
    Added current version of Unity project

---
+++ Assets/_Scripts/HouseBehaviour.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision");
+        if (other.gameObject.CompareTag("Cannonball"))
+        {
+            Debug.Log("Cannonball");
+            foreach (Transform child in transform)
+            {
+                child.gameObject.AddComponent<Rigidbody>();
+                child.gameObject.AddComponent<BoxCollider>();
+            }
+        }
+    }
+}
